a76cb5077d2eba72ee57b4456b6286318140f236
oybekweb9646 <oybekshodmonov9646@gmail.com>
2022-09-23 11:34:12 +0500

    all changes

--- src/constrollers/contact/contact.ts
+++ src/constrollers/contact/contact.ts
@@ -2,18 +2,26 @@ import TelegramBot, { Message } from "node-telegram-bot-api";
 
 import { queryRow } from "../../lib/postgres";
 import { setSettingsKeyboard } from "../../keyboards/keyboards";
+import data from "../../locales/locales.json";
+import { Translate, User } from "../../types/types";
+import { saveStepMovement } from "../../functions/functions";
 
 export async function setPhoneNumber(msg: Message, bot: TelegramBot) {
 	const chat_id: number = msg.chat.id;
 	const phone = msg.contact?.phone_number;
-	await queryRow("UPDATE users SET phone_number = $2, step = $3 WHERE chat_id = $1", [
+	await queryRow("UPDATE users SET phone_number = $2, current_step_id = $3 WHERE chat_id = $1", [
 		chat_id,
 		phone,
-		"setPhone",
+		3,
 	]);
+
+	const user: User = await queryRow("SELECT * FROM users WHERE chat_id = $1", [chat_id]);
+	await saveStepMovement(chat_id, user.current_step_id, 3);
+
+	const all: Translate = data[user.lang];
 	//:TODO postgres update bo'lmasa ham error qaytmaydi shuni tekshirish
 
-	await bot.sendMessage(chat_id, "Register succesfull", {
+	await bot.sendMessage(chat_id, all.fullRegister.text, {
 		reply_markup: {
 			keyboard: setSettingsKeyboard,
 			resize_keyboard: true,
@@ -24,11 +32,15 @@ export async function setPhoneNumber(msg: Message, bot: TelegramBot) {
 export async function updatePhoneNumber(msg: Message, bot: TelegramBot) {
 	const chat_id: number = msg.chat.id;
 	const phone = msg.contact?.phone_number || msg.text;
-
-	await queryRow("UPDATE users SET phone_number = $2 WHERE chat_id = $1", [chat_id, phone]);
+	const user: User = await queryRow(
+		"UPDATE users SET phone_number = $2 WHERE chat_id = $1 RETURNING *",
+		[chat_id, phone]
+	);
+	await saveStepMovement(chat_id, user.current_step_id, 4);
 	//:TODO postgres update bo'lmasa ham error qaytmaydi shuni tekshirish
+	const all: Translate = data[user.lang];
 
-	await bot.sendMessage(chat_id, "Update succesfull", {
+	await bot.sendMessage(chat_id, all.language.success, {
 		reply_markup: {
 			keyboard: setSettingsKeyboard,
 			resize_keyboard: true,
--- src/constrollers/start/start.ts
+++ src/constrollers/start/start.ts
@@ -2,31 +2,34 @@ import TelegramBot, { Message } from "node-telegram-bot-api";
 
 import { queryRow } from "../../lib/postgres";
 import { setPhoneKeyboard } from "../../keyboards/keyboards";
+import data from "../../locales/locales.json";
+import { Translate, User } from "../../types/types";
+import { saveStepMovement } from "../../functions/functions";
 
 export async function startController(msg: Message, bot: TelegramBot): Promise<void> {
 	const chat_id: number = msg.chat.id;
-	await queryRow(
-		"INSERT INTO users (chat_id,lang,first_name,last_name,username,step) VALUES($1,$2,$3,$4,$5,$6)",
+
+	const user: User = await queryRow(
+		"INSERT INTO users (chat_id,lang,first_name,last_name,username,current_step_id) VALUES($1,$2,$3,$4,$5,$6) RETURNING *",
 		[
 			chat_id,
 			msg.from?.language_code,
 			msg.from?.first_name,
 			msg.from?.last_name,
 			msg.from?.username,
-			"started",
+			2,
 		]
 	);
 
-	await bot.sendMessage(
-		chat_id,
-		"Assalom alekum botimizga xush kelibsiz! Telefon raqamingizni bizga yuborish  uchun ⬇️ tugmani bosing",
-		{
-			reply_markup: {
-				keyboard: setPhoneKeyboard,
-				resize_keyboard: true,
-			},
-		}
-	);
+	await saveStepMovement(chat_id, user.current_step_id, 3);
+	const all: Translate = data[user.lang];
+
+	await bot.sendMessage(chat_id, all.start.text, {
+		reply_markup: {
+			keyboard: setPhoneKeyboard,
+			resize_keyboard: true,
+		},
+	});
 
 	// return await bot.sendMessage(chatID, locales.initial.text, sendMsg("initial.text"));
 }
